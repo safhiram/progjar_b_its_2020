47eab232f7b56f0c03f9150951aecd66ee7e1f3e
Safhira Maharani <safhira.maharani14@gmail.com>
2020-04-12 16:40:24 +0700

    done tugas 6

---
+++ tugas6/client.py
@@ -0,0 +1,25 @@
+import json
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('127.0.0.1', 10001)
+print(f"connecting to {server_address}")
+sock.connect(server_address)
+try:
+    #pengiriman
+    data = 'GET / HTTP/1.0'+"\r\n\r\n"
+    sock.send(data.encode())
+
+    while True:
+       dataa = sock.recv(32)
+       receivemsg = ""
+       if (dataa):
+           receivemsg = dataa.decode()  #data harus didecode agar dapat di operasikan dalam bentuk string
+           if receivemsg[-4:]=='\r\n\r\n':
+               print("Isi Pesan:" + receivemsg)
+               break
+
+finally:
+    print("closing")
+    sock.close()
+
